fc879b8898f835e4002bb31a9076c62e2dbd280a
Nora Burkhardt <39055470+yaigrie@users.noreply.github.com>
2024-02-14 17:17:44 +0100

    3d tests

--- static/js/script.js
+++ static/js/script.js
@@ -6,30 +6,53 @@ const canvas = document.querySelector('canvas.canvas__hero')
 // Scene
 const scene = new THREE.Scene()
 
-const sphereGeometry = new THREE.TorusKnotGeometry( 0.5, 0.2, 100, 8 )
 const material = new THREE.MeshNormalMaterial()
 
-const sphere = new THREE.Mesh(sphereGeometry, material)
+// Objects
+const objectsDistance = 4
+const mesh1 = new THREE.Mesh(
+    new THREE.TorusGeometry(1, 0.4, 16, 60),
+    material
+)
+const mesh2 = new THREE.Mesh(
+    new THREE.ConeGeometry(1, 2, 32),
+    material
+)
+const mesh3 = new THREE.Mesh(
+    new THREE.TorusKnotGeometry(0.8, 0.35, 100, 16),
+    material
+)
 
-scene.add(sphere);
+mesh1.position.x = - 2
+mesh2.position.x = - 2
+mesh3.position.x = 2
+
+mesh1.position.y = - objectsDistance * 0
+mesh2.position.y = - objectsDistance * 1
+mesh3.position.y = - objectsDistance * 2
+
+scene.add(mesh1, mesh2, mesh3)
+
+const sectionMeshes = [ mesh1, mesh2, mesh3 ]
 
 const sizes = {
-    width: 560,
-    height: 560
+    width: window.innerWidth,
+    height: window.innerHeight
 }
 
 // Base camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
 camera.position.x = 0
 camera.position.y = 0
-camera.position.z = 2
+camera.position.z = 4
 scene.add(camera)
 
 /**
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
+    canvas: canvas,
+    alpha: true
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
@@ -43,9 +66,9 @@ const tick = () =>
 {
     const elapsedTime = clock.getElapsedTime()
 
-    sphere.rotation.y = 0.1 * elapsedTime
+    mesh1.rotation.y = 0.1 * elapsedTime
 
-    sphere.rotation.x = -0.15 * elapsedTime
+    mesh1.rotation.x = -0.15 * elapsedTime
 
     // Render
     renderer.render(scene, camera)
