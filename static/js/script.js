32b567a2aebbcd2606296b82002b4140861fbef8
Nora Burkhardt <39055470+yaigrie@users.noreply.github.com>
2024-02-14 17:32:44 +0100

    3d test

--- static/js/script.js
+++ static/js/script.js
@@ -14,38 +14,53 @@ const mesh1 = new THREE.Mesh(
     new THREE.TorusGeometry(1, 0.4, 16, 60),
     material
 )
+
 const mesh2 = new THREE.Mesh(
-    new THREE.ConeGeometry(1, 2, 32),
-    material
-)
-const mesh3 = new THREE.Mesh(
     new THREE.TorusKnotGeometry(0.8, 0.35, 100, 16),
     material
 )
 
 mesh1.position.x = - 2
-mesh2.position.x = - 2
-mesh3.position.x = 2
+mesh2.position.x = 2
 
-mesh1.position.y = - objectsDistance * 0
-mesh2.position.y = - objectsDistance * 1
-mesh3.position.y = - objectsDistance * 2
+mesh1.position.y = - objectsDistance * 0.25
+mesh2.position.y = - objectsDistance * 1.75
 
-scene.add(mesh1, mesh2, mesh3)
 
-const sectionMeshes = [ mesh1, mesh2, mesh3 ]
+
+scene.add(mesh1, mesh2)
+
+const sectionMeshes = [ mesh1, mesh2 ]
 
 const sizes = {
     width: window.innerWidth,
     height: window.innerHeight
 }
 
+window.addEventListener('resize', () =>
+{
+    // Update sizes
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+
+    // Update camera
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
+
+    // Update renderer
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
+
 // Base camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
-camera.position.x = 0
-camera.position.y = 0
-camera.position.z = 4
-scene.add(camera)
+const cameraGroup = new THREE.Group()
+scene.add(cameraGroup)
+
+// Base camera
+const camera = new THREE.PerspectiveCamera(35, sizes.width / sizes.height, 0.1, 100)
+camera.position.z = 6
+cameraGroup.add(camera)
+
 
 /**
  * Renderer
@@ -57,18 +72,51 @@ const renderer = new THREE.WebGLRenderer({
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
+let scrollY = window.scrollY
+let currentSection = 0
+
+window.addEventListener('scroll', () =>
+{
+    scrollY = window.scrollY
+    const newSection = Math.round(scrollY / sizes.height)
+
+    if(newSection != currentSection)
+    {
+        currentSection = newSection
+
+        gsap.to(
+            sectionMeshes[currentSection].position,
+            {
+                duration: 1.5,
+                ease: 'ease',
+                z: '+1'
+                
+            }
+        )
+    }
+})
 /**
  * Animate
  */
 const clock = new THREE.Clock()
+let previousTime = 0
 
 const tick = () =>
 {
     const elapsedTime = clock.getElapsedTime()
+    const deltaTime = elapsedTime - previousTime
+    previousTime = elapsedTime
 
-    mesh1.rotation.y = 0.1 * elapsedTime
+    // Animate camera
+    camera.position.y = - scrollY / sizes.height * objectsDistance
 
-    mesh1.rotation.x = -0.15 * elapsedTime
+
+    // Animate meshes
+    for(const mesh of sectionMeshes)
+    {
+        mesh.rotation.x += deltaTime * 0.1
+        mesh.rotation.y += deltaTime * 0.12
+    }
 
     // Render
     renderer.render(scene, camera)
